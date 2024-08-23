da28ff9fb0a14b4cf038bcc7da213b5c72e2eeff
R4finh4sz <11231100972@alunos.umc.br>
2024-08-23 16:02:20 -0300

    Desafio em desenvolvimento

--- src/index.js
+++ src/index.js
@@ -1,1 +1,37 @@
-let 
+let xp = 1150
+let name = "Rafael"
+let nivel;
+
+if ( xp <1000) {
+    let nivel = "Ferro"
+    console.log(`O Herói de nome ${name}\n está no nível de Nível: ${nivel}`)
+}
+else if (xp => 1001 && xp <= 2000) {
+    let nivel = "Bronze"
+    console.log(`O Herói de nome ${name}\n está no nível de Nível: ${nivel}`)
+}
+else if ( xp =>2001 && xp <= 5000) {
+    let nivel = "Prata"
+    console.log(`O Herói de nome ${name}\n está no nível de Nível: ${nivel}`)
+}
+else if ( xp => 5001 && xp <= 7000) {
+    let nivel = "Ouro"
+    console.log(`O Herói de nome ${name}\n está no nível de Nível: ${nivel}`)
+}
+else if ( xp => 7001 && xp <= 8000) {
+    let nivel = "Platina"
+    console.log(`O Herói de nome ${name}\n está no nível de Nível: ${nivel}`)
+}
+else if ( xp => 8001 && xp <= 9000) {
+    let nivel = "Ascendente"
+    console.log(`O Herói de nome ${name}\n está no nível de Nível: ${nivel}`)
+}
+else if ( xp => 9001 && xp <= 10000) {
+    let nivel = "Imortal"
+    console.log(`O Herói de nome ${name}\n está no nível de Nível: ${nivel}`)
+}
+else if ( xp => 10001) {
+    let nivel = "Radiante"
+    console.log(`O Herói de nome ${name} está no nível de Nível: ${nivel}`)
+}   
+else;
